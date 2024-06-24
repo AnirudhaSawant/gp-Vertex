db3706c6809b6d8a1e1144e1ba5490fdca2fc4d8
Anirudha_20 <anirudhasawant02.as@gmail.com>
2024-06-24 17:35:17 +0530

    Vertex day-1

---
+++ src/main/java/com/globalpayex/MultiOperationalsVerticle.java
@@ -0,0 +1,33 @@
+package com.globalpayex;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MultiOperationalsVerticle extends AbstractVerticle {
+
+    private int a = 10;
+    private int b = 5;
+
+    private static final Logger logger = LoggerFactory.getLogger(MultiOperationalsVerticle.class);
+
+    @Override
+    public void start() throws Exception {
+        logger.info("Verticle Starts!");
+        vertx.setTimer(5000, id1 -> {
+            int result = a + b;
+            logger.info("Addition: {}", result);
+            vertx.setTimer(3000, id2 -> {
+                int multResult = a * b + result;
+                logger.info("Multiplication: {}", multResult);
+            });
+        });
+    }
+
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new MultiOperationalsVerticle());
+    }
+}
---
+++ src/main/java/com/globalpayex/MultiOperationsVertex.java
@@ -0,0 +1,33 @@
+package com.globalpayex;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Scanner;
+
+public class MultiOperationsVertex {
+
+    private static final Logger logger = LoggerFactory.getLogger(WinnerGreeterAsync.class);
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        Scanner s = new Scanner(System.in);
+
+        while (true) {
+            logger.info("Enter the value of a : ");
+            int a = s.nextInt();
+            logger.info("Enter the value of b : ");
+            int b = s.nextInt();
+            vertx.setTimer(4000, id -> performAddition(a,b));
+            vertx.setTimer(3000, id -> performMultiplication(a,b));
+        }
+    }
+
+    public static void performAddition(int a, int b) {
+        logger.info("Addition : " + (a + b));
+    }
+
+    public static void performMultiplication(int a, int b) {
+        logger.info("Multiplication : " + (a * b));
+    }
+}
+ 
